cead47d39a4a97d51694f628898f0981b405ebaf
Arbaz8652 <105266157+Arbaz8652@users.noreply.github.com>
2022-08-24 00:45:09 +0530

    Add files via upload

---
+++ InhetiranceHirarchy22_08_22/src/Cd.java
@@ -0,0 +1,23 @@
+
+public class Cd extends MediaItem{
+	
+	private String artist;
+	private String genre;
+	
+	
+	public Cd(int id, int copy, String name, int runTime, String artist, String genre) {
+		super(id, copy, name, runTime);
+		this.artist=artist;
+		this.genre=genre;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void getArtist() {
+		System.out.println("Artist : "+artist);
+	}
+	
+	public void getGenre() {
+		System.out.println("Genre : "+genre);
+	}
+
+}
