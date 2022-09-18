c2aa2b99a31293da56c3d96007ca94e44772b29e
Arbaz8652 <105266157+Arbaz8652@users.noreply.github.com>
2022-09-18 16:31:26 +0530

    Update ExistanceOfFile.java

--- Practice14-09-22/src/com/example/ExistanceOfFile.java
+++ Practice14-09-22/src/com/example/ExistanceOfFile.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class ileReader {
+public class FileReader {
 
 	public static void main(String[] args) throws IOException {
 
