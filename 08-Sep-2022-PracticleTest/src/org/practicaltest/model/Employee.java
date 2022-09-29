056255c9337664e1e54ce396b3c46e331605abab
Arbaz Pathan <105266157+Arbaz8652@users.noreply.github.com>
2022-09-29 23:28:43 +0530

    Add files via upload

---
+++ 08-Sep-2022-PracticleTest/src/org/practicaltest/model/Employee.java
@@ -0,0 +1,34 @@
+package org.practicaltest.model;
+
+public class Employee {
+	private String name;
+	private String empId;
+	private double salary;
+	
+	public Employee(){
+		
+	}
+	public Employee(String name, String empId, double salary){
+		this.empId=empId;
+		this.name=name;
+		this.salary=salary;
+	}
+	public String getEmpId() {
+		return empId;
+	}
+	public void setempId(String empId) {
+		this.empId=empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	
+	public String toString() {
+		return empId+" "+name+" "+salary;
+	}
+	
+	
+}
